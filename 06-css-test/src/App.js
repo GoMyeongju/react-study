3b71098cf2b96c74d8640ac95471571116b3688b
Myeongju-Go <mj.go@purple-nest.com>
2021-01-23 01:57:31 +0900

    일반적인 css

---
+++ 06-css-test/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from './Button3';
+import Box from './Box4';
+
+export default function App() {
+  return (
+    <div>
+    <Button size="big"/>
+    <Button size="small"/>
+    <Box size="big"/>
+    <Box size="small"/>
+    </div>
+  );
+}
