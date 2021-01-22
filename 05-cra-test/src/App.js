b35bfeb0dd3a45bdf5578ebacebad104ba030304
Myeongju-Go <mj.go@purple-nest.com>
2021-01-23 01:53:21 +0900

    create-react-app 사용

---
+++ 05-cra-test/src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import smallImage from './small.jpeg';
+import bigImage from './big.jpeg';
+import TodoList from './TodoList';
+import './test.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList />
+      <img src={bigImage} alt="big"></img>
+      <img src={smallImage} alt="small"></img>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hello React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+console.log(`NODE_ENV = ${process.env.NODE_ENV}`);
+console.log(`REACT_APP_DATA_API = ${process.env.REACT_APP_DATA_API}`);
+console.log(`REACT_APP_LOGIN_API = ${process.env.REACT_APP_LOGIN_API}`);
+
+export default App;
